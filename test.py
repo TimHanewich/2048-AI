c5dff30d525c10ebf25e73992a8702f1d6519bd4
Tim Hanewich <tahanewich@live.com>
2023-01-28 23:27:00 -0500

    Changed path

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import Py2048_Engine.Game
 import tools
 import ai_tools
 
-model_dir_path:str = r"C:\Users\timh\Downloads\tah\2048-ai\models\2023-01-28 22-36-57.175257"
+model_dir_path:str = r"C:\Users\timh\Downloads\tah\2048-ai\models\2023-01-29 03-41-48.733715"
 
 # load the model
 model:tf.keras.Sequential = tf.keras.models.load_model(model_dir_path)
