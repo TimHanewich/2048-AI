7cdb9fd1bbc4f8276a9789df02551de359353495
Tim Hanewich <tahanewich@live.com>
2023-01-30 23:40:58 -0500

    Changed save directory

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ print("Model assembled.")
 
 # training params
 save_model_every_seconds = 600
-save_to_directory = r"C:\Users\timh\Downloads\tah\2048-ai\models"
+save_to_directory = r"C:\Users\timh\Downloads\tah\2048-ai\models2"
 
 
 # Train
