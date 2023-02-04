21c22acc64b8c040c5d86802e802682a51419616
Tim Hanewich <tahanewich@live.com>
2023-02-04 15:05:42 -0500

    Updated target

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import datetime
 import ai_tools
 
 # load model?
-model_path:str = r"C:\Users\timh\Downloads\tah\2048-ai\models4\2023-02-03 20-55-15.539040"
+model_path:str = r"C:\Users\timh\Downloads\tah\2048-ai\models4\2023-02-04 15-28-39.432783"
 model:tensorflow.keras.Sequential = None
 if model_path != None and model_path != "":
     print("Loading model from '" + model_path + "'")
